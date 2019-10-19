cf554a5f601707601c66b86cd1b539c23a002ed9
Guilherme Palma <gomes.lmc@gmail.com>
2019-10-19 16:50:53 -0300

    Cria o projeto

---
+++ sassa-cake-api/src/main/java/com/guipalm4/sassacake/SassaCakeApplication.java
@@ -0,0 +1,13 @@
+package com.guipalm4.sassacake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SassaCakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SassaCakeApplication.class, args);
+	}
+
+}
---
+++ sassa-cake-api/src/test/java/com/guipalm4/sassacake/SassaCakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guipalm4.sassacake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SassaCakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
